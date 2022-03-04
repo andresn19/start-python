9a4e7940ff390824709967c573aaba8b15090040
Andres Nieto <andres@localhost.localdomain>
2022-03-03 23:27:13 -0400

    nueva propiedad en las cadenas

--- variables/strings.py
+++ variables/strings.py
@@ -3,6 +3,8 @@ varA= "cadena"
 print (varA)
 print (type(varA))
 print (len(varA))
+print (varA[2])
+print (varA[-2])
 
 
 ######## 'type' se utiliza para ver el tipo de variable en este caso ######
