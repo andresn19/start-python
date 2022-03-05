8f224de62e3c1ef1ad09eef8eae4116a042699e5
Andres Nieto <andresn19@gmail.com>
2022-03-05 00:04:27 -0400

    Indexing de listas

--- listas/listas1.py
+++ listas/listas1.py
@@ -1,6 +1,10 @@
 list1 = [1,2,3]
 list2 = ["cambur", "fresa","naranja","melocoton"]
+lst = ['statics', 'strengths', 'dynamics']
+
 print(list1)
 print(list2)
 print(type(list1))
 print (type(list2))
+print(lst[0])
+print(lst[2])
