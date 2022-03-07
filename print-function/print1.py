4afbd0418f9c776569de0410d88e822efb59293f
Andres Nieto <andresn19@gmail.com>
2022-03-06 23:13:42 -0400

    funcion format() + numeros flotantes

---
+++ print-function/print1.py
@@ -0,0 +1,14 @@
+###### vamos a dar varios formatos con la propiedad .format() {}####
+
+from django.http import JsonResponse
+from matplotlib.colors import same_color
+
+
+print ("Los días de la semana son {} {} {} {} y {}".format ("lunes,","martes,","miercoles," ,"jueves,","viernes"))
+print ("3 palabras {2} {1} {0}" .format ("caja","raton","carrera"))
+print ("3 palabras {b} {c} {r}" .format (c="caja",r="raton",b="carrera"))
+
+name = "sam"
+age = 15
+print (f"hola, mi nombre es {name},y tengo {age} de edad")
+print ("hola mi nombre es {}" .format(name))
