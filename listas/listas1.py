b0b002b8ce6f9e2d063a645b27271f995d2b03f4
Andres Nieto <andresn19@gmail.com>
2022-03-06 21:00:45 -0400

    Otro hello en ramab

---
+++ listas/listas1.py
@@ -0,0 +1,15 @@
+list1 = [1,2,3]
+list2 = ["cambur", "fresa","naranja","melocoton"]
+lst = ['statics', 'strengths', 'dynamics']
+
+print(list1)
+print(list2)
+print(type(list1))
+print (type(list2))
+print(lst[0])
+print(lst[2])
+print (len(list2))
+print (len(list1))
+print ("hola esposa")
+print("otro hello world") 
+print ("hello ramab")
