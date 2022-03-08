b91ef2717652e1098436de414e49743288069a23
Andres Nieto <andresn19@gmail.com>
2022-03-07 20:47:39 -0400

    Eliminando

--- print-function/print1.py
+++ print-function/print1.py
@@ -1,9 +1,4 @@
 ###### vamos a dar varios formatos con la propiedad .format() {}####
-
-from django.http import JsonResponse
-from matplotlib.colors import same_color
-
-
 print ("Los días de la semana son {} {} {} {} y {}".format ("lunes,","martes,","miercoles," ,"jueves,","viernes"))
 print ("3 palabras {2} {1} {0}" .format ("caja","raton","carrera"))
 print ("3 palabras {b} {c} {r}" .format (c="caja",r="raton",b="carrera"))
