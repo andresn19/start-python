3defd932c6ca028047bb849bc3e97e0442f58304
Andres Nieto <andresn19@gmail.com>
2022-03-10 22:46:58 -0400

    Diccionarios aprendiendo

---
+++ diccionarios/dictionaries.py
@@ -0,0 +1,12 @@
+diccionario1= {'key1':'value1', 'key2':'value2'}
+print (diccionario1) 
+
+print (diccionario1['key1'])
+print (diccionario1['key2'])
+dict_2 = {'k1':123, 'k2':[0,1,2], 'k3':{'inside':100}}
+print (dict_2)
+
+print (dict_2['k3']['inside'])
+print (diccionario1.values())
+print (dict_2.values())
+print (dict_2.keys())
