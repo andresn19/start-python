ba068e527280a81aa4e0b246ee12d1058a7c356c
Andres Nieto <andresn19@gmail.com>
2022-03-20 09:17:39 -0400

    Promedios y bucles

---
+++ Loops/bucle-for.py
@@ -0,0 +1,16 @@
+for x in range(2,20,2):
+  print("todos estos numeros son pares",x)
+
+c=0
+for i in range(7):
+    c=c+1
+    print ("hola mundo " + str(c))
+
+
+adj = ["red", "big", "tasty"]
+fruits = ["apple", "banana", "cherry"]
+
+for x in adj:
+  for y in fruits:
+    print(x, y)
+    
--- ejercicios/promedio.py
+++ ejercicios/promedio.py
@@ -1,27 +1,26 @@
-#### promedio de 7 notas #####
+n1= int(input("nota 1:"))
+n2= int(input("nota 2: "))
+n3= int(input ("nota 3: "))
+
+promedio = (n1 + n2 + n3) / 3
+print("su promedio es: ", promedio)
+
+
+#############promediar equis notas ################################
+suma= 0
+total=0
+i=0
+promedio2=0
+
+notas= int(input("Introduzca cantidad de notas: "))
+
+for i in range(notas):
+    suma = int(input("dame calificacion: "))
+    total+= suma
+
+promedio2= total / notas
+print("tu promedio definitivo es de: ", promedio2)
 
 
 
-for i in range(7):
-    print ("hello world")
-    print (i)
-    
- 
- 
- 
- 
- 
- 
-my_list = ['pediatria','ingenieria','odontologia']
-for item in my_list:
-    print (item)
 
-
-## numeros pares de 1 al 10 #####
-c=0
-for i in range(5):
-    c=c+2
-    print (c)
-
-var = input("cual es tu edad ")
- 
