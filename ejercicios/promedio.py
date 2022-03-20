27d0c90d8d3515fa555431923c16de7dd4277679
Andres Nieto <andresn19@gmail.com>
2022-03-19 23:05:33 -0400

    ejercicio de promedio

---
+++ ejercicios/promedio.py
@@ -0,0 +1,27 @@
+#### promedio de 7 notas #####
+
+
+
+for i in range(7):
+    print ("hello world")
+    print (i)
+    
+ 
+ 
+ 
+ 
+ 
+ 
+my_list = ['pediatria','ingenieria','odontologia']
+for item in my_list:
+    print (item)
+
+
+## numeros pares de 1 al 10 #####
+c=0
+for i in range(5):
+    c=c+2
+    print (c)
+
+var = input("cual es tu edad ")
+ 
