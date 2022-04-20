a191b4884f8ff4a589e2b1ecb4df6c2a4e079008
Andres Nieto <andresn19@gmail.com>
2022-04-19 22:45:35 -0400

    bucle

--- Loops/bucle-for.py
+++ Loops/bucle-for.py
@@ -20,6 +20,16 @@ index_count= 0
 for letter in 'abcde':
   print('At index {} the letter is {}' .format(index_count,letter))
   index_count+=1
-  
+
+mylist1 = []
+
+for x in [2,4,6]:
+  for y in [1,10,1000]:
+    mylist1.append(x*y)
+
+print(mylist1)
+
+from random import shuffle
+
 
     
