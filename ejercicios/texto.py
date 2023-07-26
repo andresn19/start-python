0f4e919598a9578297347fef6dae11caedc6e2fb
Andres Nieto <andresn19@gmail.com>
2023-07-26 10:44:45 -0400

    capitalize, upper lower case

---
+++ ejercicios/texto.py
@@ -0,0 +1,16 @@
+##### practica con strings ####
+variablea="una cadena de texto"
+capi= variablea.capitalize()
+#### coloca en mayuscula la primera letra de la cadena de texto
+print(capi)
+
+
+####### colocar mayusculas todas y minusculas la cadena de caracteres ####
+
+variable_b=input("Dame cadena de caracteres: ")
+upper_var= variable_b.upper()
+lower_var= variable_b.lower()
+
+print(lower_var)
+print(upper_var)
+print (type(variable_b))
